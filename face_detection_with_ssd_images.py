266db872123d5e27d5f05193212baabe6b4a13b8
Peter Tanugraha <p.tanugraha@gmail.com>
2018-09-08 22:05:06 -0400

    Fixed file issue path

--- face_detection_with_ssd_images.py
+++ face_detection_with_ssd_images.py
@@ -27,11 +27,12 @@ import tensorflow as tf
 import cv2
 
 import _init_paths
-from src.utils import post_process_ssd_predictions,run_inference_for_single_image,load_tf_ssd_detection_graph
+from src.utils import post_process_ssd_predictions,run_inference_for_single_image_through_ssd,load_tf_ssd_detection_graph
 
 def run_tf_object_detection_images(input_graph,image_tensor, tensor_dict,path_to_images_dir=None):
 
   image_list = glob.glob(path_to_images_dir+'*.jpg')
+  print(image_list)
 
   with input_graph.as_default():
     with tf.Session() as sess:
@@ -39,7 +40,7 @@ def run_tf_object_detection_images(input_graph,image_tensor, tensor_dict,path_to
         while True:
             image = cv2.imread(cur_image_path)
             image_np = (cv2.cvtColor(image, cv2.COLOR_BGR2RGB)).astype(np.uint8) #Convert to RGB and convert to uint8
-            output_dict = run_inference_for_single_image(sess,image_np,image_tensor,tensor_dict)
+            output_dict = run_inference_for_single_image_through_ssd(sess,image_np,image_tensor,tensor_dict)
             dets = post_process_ssd_predictions(image_np,output_dict,threshold=0.25)
 
             for cur_det in dets:
@@ -61,7 +62,7 @@ def run_tf_object_detection_images(input_graph,image_tensor, tensor_dict,path_to
   return
 
 if __name__ == "__main__":
-  PATH_TO_FROZEN_GRAPH = './model/ssd_mobilenet_v1_focal_loss_face_mark_2.pb'
+  PATH_TO_FROZEN_GRAPH = './model/ssd_models/ssd_mobilenet_v1_focal_loss_face_mark_2.pb'
   PATH_TO_IMAGES_DIR = './images/'
 
   main_graph = tf.Graph()
